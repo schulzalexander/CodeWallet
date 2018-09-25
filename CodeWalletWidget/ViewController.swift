1608116ca4a94aa2aeb34e1bfe806b9773c19994
Alexander Schulz <alexanderschulz94@gmx.de>
2018-09-25 17:57:11 +0200

    Initial Commit

---
+++ CodeWalletWidget/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeWalletWidget
+//
+//  Created by Alexander Schulz on 25.09.18.
+//  Copyright © 2018 Alexander Schulz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
