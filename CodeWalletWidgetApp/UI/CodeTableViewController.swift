d5bb508382e5b8eca9e86eb2cc102f90924f98b5
Alexander Schulz <alexanderschulz94@gmx.de>
2019-07-21 14:12:38 +0200

    v 1.1.2

--- CodeWalletWidgetApp/UI/CodeTableViewController.swift
+++ CodeWalletWidgetApp/UI/CodeTableViewController.swift
@@ -35,16 +35,16 @@ class CodeTableViewController: UIViewController {
 		isSideMenuHidden = false
 		
 		// Preprocessor flag not working yet
-		#if SCREENSHOTS || true
-			CodeManager.shared.deleteAllCodes()
-			let code = Code(name: "Flight Ticket", value: "10985328140279", type: .pdf417, logo: nil)
-			code.id = "123"
-			CodeManager.shared.addCode(code: Code(name: "Coupon", value: "10985328140279", type: .qr, logo: nil))
-			CodeManager.shared.addCode(code: Code(name: "Coffee Shop", value: "10985328140279", type: .code128, logo: nil))
-			CodeManager.shared.addCode(code: code)
-			CodeManagerArchive.saveCodeManager()
-			//LocationService.shared.scheduleTestNotification(code: code)
-		#endif
+//		#if SCREENSHOTS || true
+//			CodeManager.shared.deleteAllCodes()
+//			let code = Code(name: "Flight Ticket", value: "10985328140279", type: .pdf417, logo: nil)
+//			code.id = "123"
+//			CodeManager.shared.addCode(code: Code(name: "Coupon", value: "10985328140279", type: .qr, logo: nil))
+//			CodeManager.shared.addCode(code: Code(name: "Coffee Shop", value: "10985328140279", type: .code128, logo: nil))
+//			CodeManager.shared.addCode(code: code)
+//			CodeManagerArchive.saveCodeManager()
+//			//LocationService.shared.scheduleTestNotification(code: code)
+//		#endif
 		
 		tableView.delegate = self
 		tableView.dataSource = self
